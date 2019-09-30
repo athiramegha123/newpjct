0fd420597b4332657eea213388f17fe0aa66e897
athiraanirudhan555 <meghanair506@gmail.com>
2019-09-30 14:50:35 +0530

    complete

---
+++ mzc/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def indexpage(request):
+    return HttpResponse("welcome to my index page")
+
+
+def contactpage(request):
+    return HttpResponse("contactpage")
+def homepage(request):
+    return  HttpResponse("welcome to my home page")     
