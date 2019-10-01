6fe78d25fe5ccf37ffaa5e0975a9d995f6df8a77
athiramegha123 <athiramegha999@gmail.com>
2019-10-01 12:13:10 +0530

    completepjt

--- mzc/views.py
+++ mzc/views.py
@@ -5,10 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def indexpage(request):
-    return HttpResponse("welcome to my index page")
+    return render(request,"index.html")
 
 
 def contactpage(request):
     return HttpResponse("contactpage")
 def homepage(request):
-    return  HttpResponse("welcome to my home page")     
+    return  HttpResponse("welcome to my home page") 
+def indexpage1(request):
+    return HttpResponse("welcome to my index page")    
